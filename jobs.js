b98ad782207f60966709ecce38736c07243716aa
vaghasiya ridham <ridhamvaghasiya21@gmail.com>
2024-06-19 06:35:22 +0000

    update

---
+++ jobs.js
@@ -0,0 +1,33 @@
+
+const JOBS = [
+    {
+        id: 1,
+        title: 'Frontend Developer',
+        location: 'Bengaluru, India',
+        salary: '₹12,00,000',
+        posted: 'Mar 3, 2023'
+    },
+    {
+        id: 2,
+        title: 'Full Stack Developer',
+        location: 'New Delhi, India',
+        salary: '₹15,00,000',
+        posted: 'Feb 1, 2023'
+    },
+    {
+        id: 3,
+        title: 'Data Scientist',
+        location: 'San Francisco, USA',
+        salary: '$175,000',
+        posted: 'Dec 22, 2022'
+    },
+    {
+        id: 4,
+        title: 'Machine Learning Engineer',
+        location: 'Remote',
+        salary: '$80,000',
+        posted: 'Sep 19, 2022'
+    },
+]
+
+module.exports = JOBS;
